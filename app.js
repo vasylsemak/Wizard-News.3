c2cf598fa413b2985469069d82b6f39790073085
Vasyl Semak <vasylsmk1401@gmail.com>
2020-10-16 13:14:36 -0400

    create: addPost html file in views; route add to display form

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const routes = require("./routes/posts");
-
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/posts", routes);
 
-app.get("*", (req, res) => {
-  res.redirect("/posts");
-})
-
+app.get("*", (req, res) => { res.redirect("/posts") });
 
 const PORT = 1337;
 app.listen(PORT, () => {
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
-const router = Router();
+const express = require("express");
+const router = express.Router();
 const client = require("../db");
 const postList = require("../views/postList");
 const postDetails = require("../views/postDetails");
-
+const addPost = require("../views/addPost");
 
 const baseQuery = "SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n";
 
@@ -14,7 +14,11 @@ router.get("/", async (req, res, next) => {
   } catch (error) { next(error) }
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get('/add', (req, res) => {
+  res.send(addPost());
+})
+
+router.get('/:id', async (req, res, next) => {
   try {
     const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
     const post = data.rows[0];
