4af80bdac70330804ebf425c92ff4cda26beb7ee
Vasyl Semak <vasylsmk1401@gmail.com>
2020-10-16 12:14:45 -0400

    create routes/post module; add redirect for non-matching routes

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
-const client = require("./db");
-const postList = require("./views/postList");
-const postDetails = require("./views/postDetails");
+const routes = require("./routes/posts");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 
-const baseQuery = "SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n";
+app.use("/posts", routes);
 
-app.get("/", async (req, res, next) => {
-  try {
-    const data = await client.query(baseQuery);
-    res.send(postList(data.rows));
-  } catch (error) { next(error) }
-});
+app.get("*", (req, res) => {
+  res.redirect("/posts");
+})
 
-app.get("/posts/:id", async (req, res, next) => {
-  try {
-    const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
-    const post = data.rows[0];
-    res.send(postDetails(post));
-  } catch (error) { next(error) }
-});
 
 const PORT = 1337;
-
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
---
+++ routes/posts.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const router = Router();
+const client = require("../db");
+const postList = require("../views/postList");
+const postDetails = require("../views/postDetails");
+
+
+const baseQuery = "SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n";
+
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await client.query(baseQuery);
+    res.send(postList(data.rows));
+  } catch (error) { next(error) }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
+    const post = data.rows[0];
+    res.send(postDetails(post));
+  } catch (error) { next(error) }
+});
+
+module.exports = router;
